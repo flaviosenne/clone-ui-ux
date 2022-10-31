d5d5844789ccc619f5db03e63bd0afd1dfb16a63
Flavio <flaviosenne123@gmail.com>
2022-10-31 14:24:37 -0300

    criar estrutura html do ProfileData

--- github/src/components/Header/index.tsx
+++ github/src/components/Header/index.tsx
@@ -1,6 +1,13 @@
+import { Container, GithubLogo, SearchForm } from "./styles"
+
 const Header: React.FC = () => {
     return (
-        <></>
+        <Container>
+            <GithubLogo />
+            <SearchForm>
+                <input placeholder="Enter Username or Repo..." />
+            </SearchForm>
+        </Container>
     )
 }
 
--- github/src/pages/Profile/index.tsx
+++ github/src/pages/Profile/index.tsx
@@ -1,6 +1,29 @@
+import { Container, Main, LeftSide, RightSide } from "./styles"
+
+import ProfileData from '../../components/ProfileData'
+
 const Profile: React.FC = () => {
     return (
-        <></>
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData
+                        username={'flaviosenne'}
+                        name={'João Flávio'}
+                        avatarUrl={'https://avatars.githubusercontent.com/u/20491701?v=4s'}
+                        followers={50}
+                        following={10}
+                        company={'Bart Digital'}
+                        location={'Paraná, Brasil'}
+                        email={'joaodev3@gmail.com'}
+                        blog={undefined}
+                    />
+
+                </LeftSide>
+
+                <RightSide></RightSide>
+            </Main>
+        </Container>
     )
 }
 
